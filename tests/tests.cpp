2b34abbcd42e3fb5e2ceffbd3add27f99b3c9f6c
Filip Hausman <filip.hausman@gmail.com>
2019-05-21 00:23:41 +0200

    simple tests added

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,4 +1,18 @@
+#include "../la.hpp"
+
+using namespace la;
+
 int main()
 {
+    constexpr vec a{1,2,3};
+    constexpr vec b{3,4,5};
+    
+    static_assert(x(a) == 1);
+    static_assert(y(a) == 2);
+    static_assert(z(a) == 3);
+    static_assert((a == b) == false);
+    static_assert(dot(a, b) == 26);
+    static_assert(cross(a, b) == vec{-2, 4, -2});
+    
     return 0;
 }
