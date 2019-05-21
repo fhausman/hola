b913d777de882e65938b47e354ef413c4b450d1a
Filip Hausman <filip.hausman@gmail.com>
2019-05-21 12:22:50 +0200

    useful aliases

--- la.hpp
+++ la.hpp
@@ -12,27 +12,32 @@ constexpr auto dot_impl(Vec const& a, Vec const& b, std::index_sequence<I...>)
 }
 }  // namespace internal
 
-template <typename... T>
-class vec : public std::array<std::common_type_t<T...>, sizeof...(T)>
+template <typename T, size_t Size>
+class vec : public std::array<T, Size>
 {
    public:
-    constexpr static auto size = sizeof...(T);
+    constexpr static auto size = Size;
 
-    constexpr bool operator==(vec<T...> const& o) const
+    constexpr bool operator==(vec<T, Size> const& o) const
     {
         return equals(o, std::make_index_sequence<size>{});
     }
 
    private:
     template <size_t... I>
-    constexpr bool equals(vec<T...> const& o, std::index_sequence<I...>) const
+    constexpr bool equals(vec<T, Size> const& o, std::index_sequence<I...>) const
     {
         return ((std::get<I>(*this) == std::get<I>(o)) && ...);
     }
 };
 
 template <typename... T>
-vec(T&&...)->vec<T...>;
+vec(T&&...)->vec<typename std::common_type<T...>::type, sizeof...(T)>;
+
+using vec2i = vec<int, 2>;
+using vec3i = vec<int, 3>;
+using vec2f = vec<float, 2>;
+using vec3f = vec<float, 3>;
 
 template <typename Vec>
 constexpr auto x(Vec const& v)
