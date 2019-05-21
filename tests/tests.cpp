f50535e1a19e9f835850a030edea46c1c325652a
Filip Hausman <filip.hausman@gmail.com>
2019-05-21 12:58:46 +0200

    test for != operator

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -11,6 +11,7 @@ int main()
     static_assert(y(a) == 2);
     static_assert(z(a) == 3);
     static_assert((a == b) == false);
+    static_assert((a != b) == true);
     static_assert(dot(a, b) == 26);
     static_assert(cross(a, b) == vec{-2, 4, -2});
     
